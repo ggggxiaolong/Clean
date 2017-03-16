08e49cf6c40cbd0cd9d14afc28c67d4d31225e3d
mrtan <1120610800@qq.com>
2017-03-16 21:41:54 +0800

    Add some code
    fix applicationTest bug
    add retrofit lib
    add net api

---
+++ data/src/main/java/com/example/mrtan/data/executor/JobExecutor.java
@@ -0,0 +1,10 @@
+package com.example.mrtan.data.executor;
+
+import com.example.mrtan.domain.executor.ThreadExecutor;
+
+/**
+ * @author mrtan 17-3-16
+ */
+
+public class JobExecutor implements ThreadExecutor {
+}
