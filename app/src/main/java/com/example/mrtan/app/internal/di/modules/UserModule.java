38aba7673cb0bdef2f991007a8951f3eb11c5b8f
mrtan <1120610800@qq.com>
2017-04-07 09:04:59 +0800

    Add module and mapper

---
+++ app/src/main/java/com/example/mrtan/app/internal/di/modules/UserModule.java
@@ -0,0 +1,11 @@
+package com.example.mrtan.app.internal.di.modules;
+
+import dagger.Module;
+
+/**
+ * @author mrtan 17-3-25
+ */
+@Module
+public class UserModule {
+
+}
---
+++ app/src/main/java/com/example/mrtan/app/mapper/UserModelDataMapper.java
@@ -0,0 +1,41 @@
+package com.example.mrtan.app.mapper;
+
+import com.example.mrtan.app.model.ImmutableUserModel;
+import com.example.mrtan.app.model.UserModel;
+import com.example.mrtan.domain.User;
+import com.fernandocejas.arrow.checks.Preconditions;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author mrtan 17-3-18
+ */
+public class UserModelDataMapper {
+
+  private UserModelDataMapper() {
+  }
+
+  public static UserModel transform(User user) {
+    Preconditions.checkNotNull(user);
+    return ImmutableUserModel.builder()
+        .userId(user.userId())
+        .coverUrl(user.coverUrl())
+        .description(user.description())
+        .email(user.email())
+        .followers(user.followers())
+        .fullName(user.fullName())
+        .build();
+  }
+
+  public static Collection<UserModel> transform(Collection<User> users) {
+    Collection<UserModel> userModels;
+    if (users != null && !users.isEmpty()) {
+      userModels = new ArrayList<>(users.size());
+      users.forEach(user -> userModels.add(transform(user)));
+    } else {
+      userModels = Collections.EMPTY_LIST;
+    }
+    return userModels;
+  }
+}
